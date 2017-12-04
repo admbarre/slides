65a4942e2e0258310423f14e6eafcd08f63d3b6b
admbarre <barrera.adrian.m@gmail.com>
2017-12-04 10:37:28 -0800

    Added CSS animations and toggling through JS.

--- static/cycle.js
+++ static/cycle.js
@@ -2,28 +2,28 @@ var slides = document.getElementById("slider").children
 var delay = 5000 //10k ms = 10s, how long to show the slide
 var duration = 2000 //2k ms = 10s, how long the transition takes
 
+var slider_width = 640
+
 function setup(slides){
 	console.log("Setting up slides...")
-	for(let i = 0; i < slides.length; i++){
-		slides[i].style.zIndex = 0
+	num = slides.length
+	for(let i = 0; i < num; i++){
+		console.log('Z: ' + (num - i))
+		slides[i].style.zIndex = num - i
+		slides[i].classList.remove('animated')
 	}
 }
 
 function shuffle(slides){
 	console.log("Shuffling...")
 	for(let i = 0; i < slides.length; i++){
-		setTimeout(toBottom, i * delay, slides[i])
+		setTimeout(cycle, (i * (delay + duration)), slides[i])
 	}
 }
 
-function toBottom(slide){
-	console.log("Executing..." + slide)
-	slide.style.zIndex = 1
-	//Xform right
-	slide.style.zIndex = -1
-	//Xform '
+function cycle(slide){
+	slide.classList.add('animated')
 }
 
-//Main
 setup(slides)
 shuffle(slides)
