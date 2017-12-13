56524d0e5217b66be37e414508a015abf4d603a0
admbarre <barrera.adrian.m@gmail.com>
2017-12-13 10:32:37 -0800

    It works

--- static/cycle.js
+++ static/cycle.js
@@ -1,29 +1,63 @@
 var slides = document.getElementById("slider").children
-var delay = 5000 //10k ms = 10s, how long to show the slide
-var duration = 2000 //2k ms = 10s, how long the transition takes
+var ms_to_s = 1000
 
-var slider_width = 640
+// Reads in variables from CSS, so that slideshow can be edited directly from
+// CSS file and not JS
+var htmlStyles = window.getComputedStyle(document.querySelector("html"))
+var delay  = parseInt(htmlStyles.getPropertyValue('--delay')) * ms_to_s
+var duration  = parseInt(htmlStyles.getPropertyValue('--duration')) * ms_to_s
 
 function setup(slides){
-	console.log("Setting up slides...")
-	num = slides.length
-	for(let i = 0; i < num; i++){
-		console.log('Z: ' + (num - i))
-		slides[i].style.zIndex = num - i
-		slides[i].classList.remove('animated')
-	}
-}
-
-function shuffle(slides){
-	console.log("Shuffling...")
 	for(let i = 0; i < slides.length; i++){
-		setTimeout(cycle, (i * (delay + duration)), slides[i])
+		slides[i].style.zIndex = slides.length - i
 	}
 }
 
-function cycle(slide){
-	slide.classList.add('animated')
+function make_ring(start, end){
+	/* Returns a generator that functions as a ring buffer with range [start, end] */
+	return function* ring(){
+		var index = start
+		while(true){
+			if(index > end){
+				index = start
+			}
+			yield index++
+		}
+	}
 }
 
+function test_circular(){
+	var ring_one_ten = make_ring(1,10)
+	var gen = ring_one_ten()
+	for(let i=0; i<35; i++){
+		console.log(gen.next())
+	}
+}
+
+//test_circular()
 setup(slides)
-shuffle(slides)
+var ring_slide = make_ring(0,slides.length-1)
+var gen = ring_slide()
+
+function slide_up(){
+	/* Shifts all slides up to make room for bringing the top card to the bottom */
+	for(let i = 0; i < slides.length; i++){
+		slides[i].style.zIndex = parseInt(slides[i].style.zIndex) + 1
+	}
+}
+
+function move_right(){
+	slide_up()
+	index = gen.next().value
+	slide = slides[index]
+	slide.classList.add('animated')
+	setTimeout(move_down, delay + duration)
+}
+
+function move_down(){
+	slide.style.zIndex = 1	//move down
+	slide.classList.remove('animated')
+	move_right()
+}
+
+move_right()
